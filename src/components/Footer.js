cab0dd6fb4cc9e961b8061b41e01bcc82834dcd0
Vanshita1204 <83549980+Vanshita1204@users.noreply.github.com>
2023-02-04 02:30:35 +0530

    add

---
+++ src/components/Footer.js
@@ -0,0 +1,42 @@
+import "./Footer.css";
+import React from 'react';
+import {FaHome, FaPhone,FaMailBulk ,FaInstagram, FaLinkedin, FaGithub} from "react-icons/fa"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+        <div className="footer-container">
+            <div className="left">
+                <div className="location">
+                    <FaHome size={20} style={{color:"#fff", marginRight:"2rem" } }/>
+                    <div>
+                        <p>Delhi, India.</p>
+                        <p>110007</p>
+                    </div>                
+                </div>
+                <div className="phone">
+                <h4><FaPhone size={20} style={{color:"#fff", marginRight:"2rem" } }/>
+                   +91 7355042461</h4>
+
+                </div>
+                <div className="email">
+                <h4><FaMailBulk size={20} style={{color:"#fff", marginRight:"2rem" } }/>
+                   vanshitajain1204@gmail.com</h4>
+
+                </div>
+            </div>
+            <div className="right">
+                <div class="social">
+                <FaInstagram size={20} style={{color:"#fff", marginRight:"2rem" } }/>
+                   <FaLinkedin size={20} style={{color:"#fff", marginRight:"2rem" } }/>
+                   
+                   <FaGithub size={20} style={{color:"#fff", marginRight:"2rem" } }/>
+                   
+                </div>      
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Heroimg.js
@@ -0,0 +1,35 @@
+import "./Heroimg.css";
+import React from 'react';
+import Intro from "../assets/intro.jpeg";
+import { Link } from "react-router-dom";
+import "../index.css"
+const Heroimg = () => {
+  return (
+    <div className="hero">
+     
+      <div className="mask">
+        <img className="intro-img" src={Intro} alt="Intro"></img>
+      </div>
+      <div className="content">
+        <h1 className="typed-out">Hello World!</h1>
+        <h1> I AM VANSHITA.</h1>
+        <p>
+         I am a coder , skilled up with in-demand full stack development tools. I feel I have a problem-solving step by step thinking approach.I aspire to be an exceptional coder and deploy my skills into efficiency and scalability.
+        </p>
+        <div>
+        <Link to="/projects" className="btn">
+          Projects
+        </Link>
+        <Link to="/contact" className="btn btn-light">
+          Contact
+        </Link>
+      </div>
+     
+      </div>
+      
+    </div>
+  );
+};
+
+
+export default Heroimg
